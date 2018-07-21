5fa8d7ff25e72e1913f6a0a905b7bd1c116975f1
Nicole Richardson <njrichardson95@gmail.com>
2018-07-21 01:12:31 -0400

    trying to make twitter work

--- liri.js
+++ liri.js
@@ -6,26 +6,68 @@ var keys = require("./keys");
 
 var client = new Twitter(keys.Twitter);
 
-var params = {screen_name: 'rbiswas'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+var twitterKeys = keys.twitterKeys;
 
-  
-});
+/*
+* 	Read in command line arguments
+*/
+
+// Read in the command line arguments
+var cmdArgs = process.argv;
+
+// The LIRI command will always be the second command line argument
+var liriCommand = cmdArgs[2];
+
+// The parameter to the LIRI command may contain spaces
+var liriArg = '';
+for (var i = 3; i < cmdArgs.length; i++) {
+	liriArg += cmdArgs[i] + ' ';
+}
+
+// retrieveTweets will retrieve my last 20 tweets and display them together with the date
+function retrieveTweets() {
+	// Append the command to the log file
+	fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
+		if (err) throw err;
+	});
+
+	// Initialize the Twitter client
+	var client = new Twitter(twitterKeys);
+
+	// Set the 'screen_name' to my Twitter handle
+	var params = {screen_name: '_angrbrd', count: 20};
+
+	// Retrieve the last 20 tweets
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			var errorStr = 'ERROR: Retrieving user tweets -- ' + error;
+
+			// Append the error string to the log file
+			fs.appendFile('./log.txt', errorStr, (err) => {
+				if (err) throw err;
+				console.log(errorStr);
+			});
+			return;
+		} else {
+			// Pretty print user tweets
+			var outputStr = '------------------------\n' +
+							'User Tweets:\n' +
+							'------------------------\n\n';
+
+			for (var i = 0; i < tweets.length; i++) {
+				outputStr += 'Created on: ' + tweets[i].created_at + '\n' +
+							 'Tweet content: ' + tweets[i].text + '\n' +
+							 '------------------------\n';
+			}
+
+			// Append the output to the log file
+			fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outputStr + '\n', (err) => {
+				if (err) throw err;
+				console.log(outputStr);
+			});
+		}
+	});
+}
 
 
-var spotify = new Spotify(keys.Spotify);
- 
-spotify.search({ type: 'track', query: 'Hit me baby one more' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(JSON.stringify(data,null, 2)); 
-});
-
-
-
-
+retrieveTweets();
